7784e736143703171bc2d593a3ab4d512b23368b
anfegagra <pipe-777@hotmail.com>
2020-01-14 17:13:06 -0500

    se corrige ubicación de clase

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.springboot.web.app;
+package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
