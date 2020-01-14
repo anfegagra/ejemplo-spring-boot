cbf79ca3ca44b11647e833cec1ef75b27b01c7f8
anfegagra <pipe-777@hotmail.com>
2020-01-14 17:15:45 -0500

    se agregan varias rutas a un mismo método

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "/index")
+    @GetMapping({"/index", "/", "home"})
     public String index() {
         return "index";
     }
