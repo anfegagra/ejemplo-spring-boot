533660fd781afee17b9fdcc2eb7d5c162b0c75ad
Andres Felipe García Granados <garandre@colpatria.com>
2020-01-15 20:51:21 -0500

    se cambia versión de java a 1.8 y se agrega datos por el model para pasarlos a la vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @GetMapping({"/index", "/", "home"})
-    public String index() {
-        return "index";
-    }
+	@GetMapping({"/index", "/", "home"})
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Boot!");
+		return "index";
+	}
 }
