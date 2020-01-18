88a12b2184c7bd292d5afe2d7fb59db5d85d9fa8
Andres Felipe García Granados <garandre@colpatria.com>
2020-01-18 15:07:14 -0500

    se agrega @RequestMapping para mapear una ruta base a todos los métodos del controlador

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({"/index", "/", "home"})
+	@GetMapping({"/index", "/", "", "home"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Boot!");
 		return "index";
