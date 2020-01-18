88f74bcb9c26b99c56204413525aae65597b7ce2
Andres Felipe García Granados <garandre@colpatria.com>
2020-01-18 15:38:21 -0500

    se agrega el modelo usuario para retornar el perfil en otra vista

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +14,15 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Boot!");
 		return "index";
+	}
+
+	@GetMapping("/perfil")
+	public String obtenerPerfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Felipe");
+		usuario.setApellido("Garcia");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";
 	}
 }
