b4fcc4ac765e027ce1f2870436a14306471be4c6
Andres Felipe García Granados <garandre@colpatria.com>
2020-01-25 18:40:37 -0500

    se crea método handler para enviar y recibir más de un parámetro en la ruta

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploPathVariableController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploPathVariableController.java
@@ -6,14 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/string")
+@RequestMapping("/variables")
 @Controller
 public class EjemploPathVariableController {
 
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta @PathVariable");
+		return "variables/index";
+	}
+
 	@GetMapping("/path/{variable}")
-	public String obtenerVariableDePath(@PathVariable String variable, Model model) {
+	public String obtenerVariableDelPath(@PathVariable String variable, Model model) {
 		model.addAttribute("titulo", "Recibir variable del path con @PathVariable");
 		model.addAttribute("resultado", "El texto enviado en la ruta es: " + variable);
+		return "variables/ver";
+	}
+
+	@GetMapping("/path/{saludo}/{numero}")
+	public String obtenerVariablesDelPath(@PathVariable String saludo,
+		@PathVariable Integer numero, Model model) {
+		model.addAttribute("resultado",
+			"El saludo es: " + saludo + ". El número enviado es: " + numero);
 		return "variables/ver";
 	}
 }
