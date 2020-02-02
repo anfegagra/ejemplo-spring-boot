9ecf565ad45d7cfeecf9d2e3dd348bed6795522d
Andres Felipe García Granados <garandre@colpatria.com>
2020-02-02 11:59:55 -0500

    se inyectan valores a variables desde application.properties

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import com.bolsadeideas.springboot.web.app.models.Usuario;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/app")
 public class IndexController {
 
+	@Value("${indexcontroller.index.titulo}")
+	private String tituloIndex;
+
+	@Value("${indexcontroller.perfil.titulo}")
+	private String tituloPerfil;
+
+	@Value("${indexcontroller.usuarios.titulo}")
+	private String tituloUsuarios;
+
 	@GetMapping({"/index", "/", "", "home"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola Spring Boot!");
+		model.addAttribute("titulo", tituloIndex);
 		return "index";
 	}
 
@@ -25,7 +35,7 @@ public class IndexController {
 		usuario.setApellido("Garcia");
 		usuario.setCorreo("prueba@prueba.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", tituloPerfil.concat(" " + usuario.getNombre()));
 		return "perfil";
 	}
 
@@ -34,7 +44,7 @@ public class IndexController {
 		List<Usuario> usuarios = Arrays
 			.asList(new Usuario("Felipe", "Garcia", "prueba@prueba.com"),
 				new Usuario("David", "Garcia", "prueba-david@prueba.com"));
-		model.addAttribute("titulo", "Lista de usuarios");
+		model.addAttribute("titulo", tituloUsuarios);
 		model.addAttribute("usuarios", usuarios);
 		return "usuarios";
 	}
